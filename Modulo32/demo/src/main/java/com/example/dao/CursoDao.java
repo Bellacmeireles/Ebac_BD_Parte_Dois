cc26d1b0ad1cc028a60cc6f47d3e745d6bd3a5d5
Isabella Cabral <bellacabral80@gmail.com>
2023-09-13 12:50:06 -0300

    delete mod32

--- Modulo32/demo/src/main/java/com/example/dao/CursoDao.java
+++
@@ -1,5 +0,0 @@
-package Modulo32.demo.src.main.java.com.example.dao;
-
-public class CursoDao implements ICursoDao {
-    
-}
--- Modulo32/demo/src/main/java/com/example/domain/Curso.java
+++
@@ -1,5 +0,0 @@
-package Modulo32.demo.src.main.java.com.example.domain;
-
-public class Curso {
-    
-}
