06e6a699d3487d456634019b2657cb27d50fd642
Isabella Cabral <bellacabral80@gmail.com>
2023-09-27 22:35:53 -0300

    projeto 4 mod35

---
+++ Modulo35/demo/src/main/java/com/example/dao/jpa/ProdutoQuantidadeJpa.java
@@ -0,0 +1,10 @@
+package com.example.dao.jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "TB_PRODUTO_QUANTIDADE")
+public class ProdutoQuantidadeJpa {
+    
+}
