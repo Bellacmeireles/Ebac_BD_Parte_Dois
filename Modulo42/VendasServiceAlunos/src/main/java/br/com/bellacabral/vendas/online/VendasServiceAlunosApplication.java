c9db4db94545db56aec4e63ddf452d9af437e150
Isabella Cabral <bellacabral80@gmail.com>
2023-11-17 15:57:35 -0300

    atualizando vendasServices

--- Modulo42/VendasServiceAlunos/src/main/java/br/com/bellacabral/vendas/online/VendasServiceAlunosApplication.java
+++ Modulo42/VendasServiceAlunos/src/main/java/br/com/bellacabral/vendas/online/VendasServiceAlunosApplication.java
@@ -1,9 +1,16 @@
 package br.com.bellacabral.vendas.online;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@RefreshScope
+@EnableFeignClients
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class VendasServiceAlunosApplication {
 
 	public static void main(String[] args) {
